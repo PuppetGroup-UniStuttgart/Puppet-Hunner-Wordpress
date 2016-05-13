b30db979997b4f388633189e65e27c7c8ec39e6a
shruthikuki <shruthikuki@gmail.com>
2016-05-13 22:15:20 +0200

    Updation
    Inclusion of php5-mysql, cd into right folder & puppet apply

--- script.sh
+++ script.sh
@@ -5,8 +5,9 @@ apt-get -y update
 apt-get -y install puppet 
 puppet module install puppetlabs-apache
 puppet module install puppetlabs-mysql
-cp -a wordpress ~/.puppet/modules
-cd ~/.puppet/modules
+apt-get -y install php5-mysql
+cp -a wordpress /etc/puppet/modules
+cd /etc/puppet/modules
 puppet module build wordpress
 puppet module install wordpress/pkg/hunner-wordpress-1.0.0.tar.gz
-puppet module apply install.pp
+puppet apply install.pp
