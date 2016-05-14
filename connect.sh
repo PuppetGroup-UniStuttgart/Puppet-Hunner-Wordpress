85c7bc77b2527b8834a37378d56f511ddae7a8c0
shruthikuki <shruthikuki@gmail.com>
2016-05-14 22:18:26 +0200

    building wordpress during connect

--- connect.sh
+++ connect.sh
@@ -1,6 +1,13 @@
 echo apt-get -y update
 apt-get -y update
+echo cd /etc/puppet/modules
+cd /etc/puppet/modules
+echo puppet module build wordpress
+puppet module build wordpress
+echo puppet module install wordpress/pkg/hunner-wordpress-1.0.0.tar.gz
+puppet module install wordpress/pkg/hunner-wordpress-1.0.0.tar.gz
 echo puppet apply wordpress/install.pp
 puppet apply wordpress/install.pp
 echo rm /var/www/html/index.html
 rm /var/www/html/index.html
+echo Success!!!
