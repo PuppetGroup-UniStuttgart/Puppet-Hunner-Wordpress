e630c95711c73c3749fe29b1747a270984310aff
shruthikuki <shruthikuki@gmail.com>
2016-05-14 22:12:09 +0200

    Update deployDB.sh

--- deployDB.sh
+++ deployDB.sh
@@ -1,3 +1,6 @@
+echo apt-get -y update
 apt-get -y update
+echo puppet module install puppetlabs-mysql
 puppet module install puppetlabs-mysql
+echo apt-get -y install php5-mysql
 apt-get -y install php5-mysql
