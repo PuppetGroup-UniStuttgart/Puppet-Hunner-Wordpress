4d175eae155020637ea05247c5ac28cd909153ac
shruthikuki <shruthikuki@gmail.com>
2016-05-19 04:30:54 +0200

    Removal of package creation

--- connect.sh
+++ connect.sh
@@ -4,8 +4,6 @@ echo cd /etc/puppet/modules
 cd /etc/puppet/modules
 echo puppet module build wordpress
 puppet module build wordpress
-echo puppet module install wordpress/pkg/hunner-wordpress-1.0.0.tar.gz
-puppet module install wordpress/pkg/hunner-wordpress-1.0.0.tar.gz
 echo puppet apply wordpress/install.pp
 puppet apply wordpress/install.pp
 echo rm /var/www/html/index.html
